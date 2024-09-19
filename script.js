f509069ced84b9d5f5c0813b219d890a33249dd0
SriMoorthy <srimoorthy27@gmail.com>
2024-09-19 12:23:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let n = confirm("Do you want to proceed?")
+
+if(n){
+	alert("You clicked OK. Proceeding...")
+}
+else{
+	alert("You clicked Cancel. Exiting...")
+}
+
